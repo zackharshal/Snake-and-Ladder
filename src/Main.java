8f1645f25ce47b30f160b48979f5fdf914183724
zackharshal <harshalgotarne25@gmail.com>
2024-06-11 12:11:21 +0530

    Dice roll
    Added a function that return a number from 1 to 6 and printing that number

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static int random(){
+        int rand = (int) ((Math.random() *6) +1);
+        return rand;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game.");
+        int rand = random();
+        System.out.println(rand);
     }
 }
