07d36332358e72cae1f2f720e4d593454b5dbcc5
zackharshal <harshalgotarne25@gmail.com>
2024-06-11 12:26:53 +0530

    3 cases
    Checks for 3 options, player has no play, player has to move ahead with the help of ladder and player has to move back incase of snake

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,21 @@ public class Main {
         int rand = (int) ((Math.random() *6) +1);
         return rand;
     }
+    public static int random3(){
+        int rand = (int) ((Math.random() * 3) + 1);
+        return rand;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game.");
         int rand = random();
         System.out.println(rand);
+        int check = random3();
+        if(check == 1){
+            System.out.println("No play");
+        } else if (check == 2) {
+            System.out.println("Ladder");
+        }else{
+            System.out.println("Snake");
+        }
     }
 }
